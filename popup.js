2c6ddb9a2b091987d10102ba2eea79db631a92a7
Timothy Chu <timchu@google.com>
2023-01-14 21:39:47 -0800

    Minor formatting

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 console.log("Popup js working!")
 chrome.storage.local.get(["key"]).then((result) => {
-	console.log(result);
-	console.log("Value currently is " + result.key);
-	console.log(document.documentElement.innerHTML);
-	console.log(document.body.innerHTML);
-	document.body.innerHTML = '<img src="' + result.key + '">'
+  console.log(result);
+  console.log("Value currently is " + result.key);
+  console.log(document.documentElement.innerHTML);
+  console.log(document.body.innerHTML);
+  document.body.innerHTML = '<img src="' + result.key + '">'
 });
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
