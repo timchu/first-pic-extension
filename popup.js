232d07dcc47787188825637f326868c4da939ca8
Timothy Chu <timchu@google.com>
2023-01-14 22:06:11 -0800

    The X button works in a single popup instance, BUT it doesn't work if we leave the popup and come back since we didn't get rid of the item from storage. TODO: Get rid of the item from storage when the X button is hit.

--- popup.js
+++ popup.js
@@ -4,10 +4,23 @@ chrome.storage.local.get(["key"]).then((result) => {
   console.log("Value currently is " + result.key);
   console.log(document.documentElement.innerHTML);
   console.log(document.body.innerHTML);
-  document.body.innerHTML = '<img src="' + result.key + '">'
+  document.body.innerHTML = '<div class=image-container><br><div class="close-button">X</div><img src="' + result.key + '"></div>'
+  let close_buttons = document.getElementsByClassName("close-button")
+  for (i = 0; i < close_buttons.length; i++){
+    console.log("Close button obtained!");
+    close_buttons[i].addEventListener("click", removeImage);
+  }
 });
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
     // use `url` here inside the callback because it's asynchronous!
     console.log('url: ' + url);
 });
+
+
+function removeImage() {
+  console.log("Removing parentNode");
+  console.log(this.outerHTML);
+  this.parentNode.remove();
+  //element.parentNode.remove();
+};
