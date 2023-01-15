06fb9ede156b6e97479007ea50f2ffb9e3a020d9
Timothy Chu <timchu@google.com>
2023-01-15 11:48:19 -0800

    Added http:// paths to accessible sites, and removed the extra popup window. Also resized the image's max height and width.

--- popup.js
+++ popup.js
@@ -1,26 +1,16 @@
-var popupWindow = window.open(
-    chrome.runtime.getURL("normal_popup.html"),
-    "exampleName",
-    "width=400,height=400"
-);
-popupWindow.window.onload = function() { 
-  console.log(popupWindow.document.body.innerHTML);
-  popupWindow.document.body.innerHTML = 'Inner HTML set on popup worked!';
-}
 console.log("Popup js working!")
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
-    console.log('url: ' + url);
+  let url = tabs[0].url;
+  console.log('url: ' + url);
   chrome.storage.local.get([url]).then((result) => {
-  console.log("Result:" + JSON.stringify(result));
-  console.log("Result at url:" + JSON.stringify(result[url]));
-  var keyed_image_srcs = result[url];
-  for (let key in keyed_image_srcs){
-    console.log("Key : " + key);
-    console.log("Image src: " + keyed_image_srcs[key]);
-    document.body.innerHTML = '<div> Keyed <img src="' + keyed_image_srcs[key] + '"></div>'
-  }
+    console.log("Result:" + JSON.stringify(result));
+    console.log("Result at url:" + JSON.stringify(result[url]));
+    var keyed_image_srcs = result[url];
+    for (let key in keyed_image_srcs){
+      console.log("Key : " + key);
+      console.log("Image src: " + keyed_image_srcs[key]);
+      document.body.innerHTML = '<div> Keyed <img style="max-height:600px; max-width:340px; height=auto; width=auto" src="' + keyed_image_srcs[key] + '"></div>'
+    }
 //    document.body.innerHTML = '<div class=image-container><br><div class="close-button">X</div><img src="' + result.key + '"></div>'
 //    let close_buttons = document.getElementsByClassName("close-button")
 //    for (i = 0; i < close_buttons.length; i++){
